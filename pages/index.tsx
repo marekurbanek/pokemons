95a8d0a4d110dcc5301b66a170a4755019824618
Marek Urbanek <marek.urbi@gmail.com>
2022-01-31 21:30:38 +0100

    Added trpc

--- pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import { NextPage } from "next";
-
-const Home: NextPage = () => {
-  return <div className="text-2xl text-red-200">Hello world</div>;
-};
-
-export default Home;
---
+++ src/pages/index.tsx
@@ -0,0 +1,28 @@
+import { NextPage } from "next";
+import { trpc } from '@/utils/trpc';
+
+const Home: NextPage = () => {
+  const { data, isLoading } = trpc.useQuery(["hello", { text: "cośtam" }]);
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
+  if (data) {
+    return <div>{data.greeting}</div>;
+  }
+
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which Pokemon is rounder?</div>
+      <div className="p-2"></div>
+      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="p-8">Vs</div>
+        <div className="w-16 h-16 bg-red-200"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
