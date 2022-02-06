6b4a71b025e945718d79bb767425b6c853eb513c
Marek Urbanek <marek.urbi@gmail.com>
2022-02-06 13:41:41 +0100

    Added images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,40 @@
 import { NextPage } from "next";
-import { trpc } from '@/utils/trpc';
+import { trpc } from "@/utils/trpc";
+import { getOptionsForVote } from "@/utils/getRandomPokemon";
+import { useMemo } from "react";
 
 const Home: NextPage = () => {
-  const { data, isLoading } = trpc.useQuery(["hello", { text: "cośtam" }]);
+  const [first, second] = useMemo(() => getOptionsForVote(), []);
+  const firstPokemon = trpc.useQuery(["get-pokemon-by-id", { id: first }]);
+  const secondPokemon = trpc.useQuery(["get-pokemon-by-id", { id: second }]);
 
-  if (isLoading) {
-    return <div>Loading</div>;
-  }
-
-  if (data) {
-    return <div>{data.greeting}</div>;
-  }
+  if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is rounder?</div>
       <div className="p-2"></div>
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-64 h-64 flex flex-col">
+          <img
+            className="w-full"
+            src={firstPokemon.data?.sprites.front_default || undefined}
+          />
+          <div className="text-xl text-center capitalize mt-[-2rem]">
+            {firstPokemon.data?.name}
+          </div>
+        </div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-64 h-64 flex flex-col">
+          <img
+            className="w-full"
+            src={secondPokemon.data?.sprites.front_default || undefined}
+          />
+          <div className="text-xl text-center capitalize mt-[-2rem]">
+            {secondPokemon.data?.name}
+          </div>
+        </div>
+        <div className="p-2"></div>
       </div>
     </div>
   );
